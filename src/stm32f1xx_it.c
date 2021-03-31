0d6a1e4be040b9135f1b08751171228db7f2b1c1
Jan Horacek <me@apophis.cz>
2021-03-31 15:21:03 +0200

    Use SysTick for Timebase source.

--- src/stm32f1xx_it.c
+++ src/stm32f1xx_it.c
@@ -33,15 +33,6 @@ void DebugMon_Handler(void) {}
 // This function handles Pendable request for system service.
 void PendSV_Handler(void) {}
 
-void SysTick_Handler(void) {}
-
-/******************************************************************************/
-/* STM32F1xx Peripheral Interrupt Handlers                                    */
-/* Add here the Interrupt Handlers for the used peripherals.                  */
-/* For the available peripheral interrupt handler names,                      */
-/* please refer to the startup file (startup_stm32f1xx.s).                    */
-/******************************************************************************/
-
-void TIM1_UP_IRQHandler(void) {
-	HAL_TIM_IRQHandler(&htim1);
+void SysTick_Handler(void) {
+	HAL_IncTick();
 }
